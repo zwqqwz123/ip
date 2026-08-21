90315d8550eccf5a88c7944c8bd13877240874b5
zwqqwz123 <zongwei.quah@gmail.com>
2026-08-21 13:08:44 +0800

    Added echo

--- src/main/java/Woofer.java
+++ src/main/java/Woofer.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * A simple command-line chatbot that echoes commands until the user says bye.
+ */
 public class Woofer {
+    /**
+     * Starts Woofer, reads commands from standard input, and exits on "bye".
+     *
+     * @param args command-line arguments, which are not used
+     */
     public static void main(String[] args) {
         String separator = "_".repeat(100);
         String banner = " __        __   ____   ____   _____   _____   ____  \n"
@@ -10,9 +20,22 @@ public class Woofer {
         System.out.println(separator);
         System.out.println(banner);
         System.out.println("Hello! I'm Woofer.");
-        System.out.println("What can I do for you today?");
+        System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon! Woof!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            System.out.println(separator);
+
+            if ("bye".equals(command)) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(command);
+            System.out.println(separator);
+        }
     }
 }
